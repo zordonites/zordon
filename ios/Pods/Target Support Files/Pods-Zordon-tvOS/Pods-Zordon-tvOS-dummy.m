b30b626c52ea575cf942fddffd22dd0986af9e7d
Jonny Nabors <jonnynabors@gmail.com>
2019-04-15 16:35:14 -0400

    Link up appcenter library
    We are choosing to commit our Pods directory to ensure the pod artifacts
    are consistent across any machine that we may use. See this post for
    more information: https://stackoverflow.com/questions/45587084/should-we-commit-pod-files-to-version-control-system-git-or-svn

---
+++ ios/Pods/Target Support Files/Pods-Zordon-tvOS/Pods-Zordon-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Zordon_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_Zordon_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-Zordon-tvOSTests/Pods-Zordon-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Zordon_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Zordon_tvOSTests
+@end
