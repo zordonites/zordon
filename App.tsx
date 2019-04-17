51046017efb23d51b1836dc9d533b195d2d1bbe0
Jonny Nabors <jnabors@ford.com>
2019-04-17 10:00:25 -0400

    Make call to backend to retrieve vehicle location
    Signed-off-by: Chris Tobolski <ctobolsk@ford.com>

--- App.tsx
+++ App.tsx
@@ -1,31 +1,49 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
- * 
+ *
  * @format
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 interface Props {}
-export default class App extends Component<Props> {
+interface State {
+  latitude: number;
+  longitude: number;
+}
+export default class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    };
+  }
+  async componentDidMount() {
+    const response = (await axios.get(
+      "https://tmc-zordon-brain.herokuapp.com/vehicle-data"
+    )) as any;
+
+    this.setState({
+      latitude: response.data.fields.location.lat.value,
+      longitude: response.data.fields.location.lon.value
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Ben's Info</Text>
+        <Text style={styles.instructions}>Latitude: {this.state.latitude}</Text>
+        <Text style={styles.instructions}>
+          Longitude: {this.state.longitude}
+        </Text>
       </View>
     );
   }
@@ -34,18 +52,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
